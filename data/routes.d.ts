8b1a6fc22d51a7798f2b8962928ab18337d8d56c
Timothy Lowrimore <tlowrimore@salesforce.com>
2026-05-07 14:29:29 -0600

    wip: adding types for Shogun

---
+++ data/routes.d.ts
@@ -0,0 +1,24 @@
+export interface RouteDefinition {
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+  path: string
+  hasRequestBody?: true
+}
+
+export declare const transfer: Record<string, RouteDefinition>
+export declare const database: Record<string, RouteDefinition>
+export declare const backup: Record<string, RouteDefinition>
+export declare const burstStatus: Record<string, RouteDefinition>
+export declare const connectionPooling: Record<string, RouteDefinition>
+export declare const link: Record<string, RouteDefinition>
+export declare const metrics: Record<string, RouteDefinition>
+export declare const restore: Record<string, RouteDefinition>
+export declare const transferSchedule: Record<string, RouteDefinition>
+export declare const maintenance: Record<string, RouteDefinition>
+export declare const postgres: Record<string, RouteDefinition>
+export declare const postgresCredential: Record<string, RouteDefinition>
+export declare const postgresPool: Record<string, RouteDefinition>
+export declare const postgresQuota: Record<string, RouteDefinition>
+export declare const postgresSettings: Record<string, RouteDefinition>
+export declare const postgresLevel: Record<string, RouteDefinition>
+export declare const postgresDatabase: Record<string, RouteDefinition>
+export declare const redis: Record<string, RouteDefinition>
---
+++ data/types.d.ts
@@ -0,0 +1,58 @@
+
+
+export interface HerokuClient {
+  transfer: {
+
+  }
+  database: {
+
+  }
+  backup: {
+
+  }
+  burstStatus: {
+
+  }
+  connectionPooling: {
+
+  }
+  link: {
+
+  }
+  metrics: {
+
+  }
+  restore: {
+
+  }
+  transferSchedule: {
+
+  }
+  maintenance: {
+
+  }
+  postgres: {
+
+  }
+  postgresCredential: {
+
+  }
+  postgresPool: {
+
+  }
+  postgresQuota: {
+
+  }
+  postgresSettings: {
+
+  }
+  postgresLevel: {
+
+  }
+  postgresDatabase: {
+
+  }
+  redis: {
+
+  }
+}
